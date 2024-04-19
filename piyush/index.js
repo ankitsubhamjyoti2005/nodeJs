b47bbca6a6efee2db99f20097d25a640b7d61e74
ankitsubhamjyot2005 <ankitsubhamjyoti@gmail.com>
2024-04-19 15:58:09 +0530

    19-apr

--- piyush/index.js
+++ piyush/index.js
@@ -1,3 +1,10 @@
 //server
 const http = require("http");
 
+const myServer = http.createServer((req,res)=>{
+    console.log(req.headers);
+    res.end("hello from server")
+});
+
+myServer.listen(8000,()=>console.log("server started"));
+
