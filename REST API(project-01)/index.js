2ac0cd94bb6b37655ca7accc105f450b2207e028
ankitsubhamjyot2005 <ankitsubhamjyoti@gmail.com>
2024-04-30 21:27:20 +0530

    update

--- REST API(project-01)/index.js
+++ REST API(project-01)/index.js
@@ -1,7 +1,46 @@
-const express = requir('express');
-
+const express = require('express');
+const users = require("./MOCK_DATA.json")
 const app = express();
 const PORT = 8000;
 
 //Routes
-app.lishen(PORT,()=>console.log("server is started"));
+
+//GET methos
+app.get("/",(req,res)=>{
+    res.end("go to '/users'");
+})
+
+
+app.use(express.urlencoded({extended :false}));
+
+// give json respone for api request done by mobile application
+app.get("/api/users",(req,res)=>{
+    return res.json(users);
+})
+
+//give html response done by browsers
+app.get("/users", (req, res) => {
+    const html = `
+    <ul>
+        ${users.map((user) => `<li>${user.first_name}</li>`).join('')}
+    </ul>
+    `;
+    res.send(html);
+});
+
+
+//Post method
+app.get("/api/users/:id",(req,res)=>{
+    const id = Number(req.params.id);
+    const user = users.find((user)=> user.id === id);
+    return res.json(user);
+    console.log(user);
+})
+
+app.post("/api/users",(req,res) => {
+    const body = req.body;
+    console.log(body);
+    
+})
+
+app.listen(PORT,()=>console.log("server is started"));
